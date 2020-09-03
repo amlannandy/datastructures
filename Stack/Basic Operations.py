54dc9301a1bd2f54c16c37911d2b5cf8609537fd
Amlan Kumar Nandy <amlannandy5@gmail.com>
2020-09-03 15:19:55 +0530

    first commit

---
+++ Stack/Basic Operations.py
@@ -0,0 +1,30 @@
+from Stack import Stack
+
+
+def printInstructions():
+    print('Stack Tech')
+    print('Command List')
+    print('1. Push')
+    print('2. Pop')
+    print('3. Peek')
+    print('4. Display')
+    print('5. Exit')
+
+
+stack = Stack()
+
+while True:
+    choice = int(input('Enter your command: '))
+    if choice == 0:
+        printInstructions()
+    elif choice == 1:
+        item = int(input('Enter item: '))
+        stack.push(item)
+    elif choice == 2:
+        stack.pop()
+    elif choice == 3:
+        stack.peek()
+    elif choice == 4:
+        stack.display()
+    elif choice == 5:
+        break
